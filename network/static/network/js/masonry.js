e33edbf2f5c0a1691cb172b98457447422e84815
John Kevin Francisco <johnkev.francisco@gmail.com>
2020-10-26 13:19:37 +0800

    release v1

--- network/static/network/js/masonry.js
+++ network/static/network/js/masonry.js
@@ -1,4 +1,5 @@
-document.addEventListener("DOMContentLoaded", function() {
+//document.addEventListener("DOMContentLoaded", function() {
+var $grid = $('.grid').imagesLoaded( function() {
     $('.grid').masonry({
     itemSelector: '.grid-item', // use a separate class for itemSelector, other than .col-
     columnWidth: '.grid-sizer',
