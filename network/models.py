cd1abf1027f7da9e4046be730c188c4762dcbc7a
John Kevin Francisco <johnkev.francisco@gmail.com>
2021-11-27 10:30:23 +0800

    test if AbstractUser is causing deployment error

--- network/models.py
+++ network/models.py
@@ -4,68 +4,70 @@ from django.forms import ModelForm
 from django import forms
 
 
-class User(AbstractUser):
+# class User(AbstractUser):
+#     pass
+class User(models.Model):
     pass
+
 
 class Post(models.Model):
     owner = models.ForeignKey(
-                        User,
-                        related_name='posts_posted',
-                        on_delete=models.CASCADE,
-                    )
+        User,
+        related_name="posts_posted",
+        on_delete=models.CASCADE,
+    )
 
     title = models.CharField(
-                        max_length=111,
-                    )
+        max_length=111,
+    )
     text = models.CharField(
-                        max_length=555,
-                    )
-    image = models.ImageField(
-                        upload_to='network/',
-                        blank=True
-                    )
-    timestamp = models.DateTimeField(
-                        auto_now=True
-                    )
+        max_length=555,
+    )
+    image = models.ImageField(upload_to="network/", blank=True)
+    timestamp = models.DateTimeField(auto_now=True)
+
     def __str__(self):
         return f"{self.owner} posted {self.title}"
+
 
 class PostForm(ModelForm):
     class Meta:
         model = Post
-        #fields = '__all__'
-        exclude = ['owner']
-        widgets = {
-            'text': forms.widgets.Textarea()
-        }
+        # fields = '__all__'
+        exclude = ["owner"]
+        widgets = {"text": forms.widgets.Textarea()}
+
     # No changes for now, later on this can change
     # If truly no changes, let's try using thet ModelForm Factory function
 
+
 class Like(models.Model):
     post = models.ForeignKey(
-                        Post,
-                        related_name='likes',
-                        on_delete=models.CASCADE,
-                    )
+        Post,
+        related_name="likes",
+        on_delete=models.CASCADE,
+    )
     liker = models.ForeignKey(
-                        User,
-                        related_name='liked',
-                        on_delete=models.CASCADE,
-                    )
+        User,
+        related_name="liked",
+        on_delete=models.CASCADE,
+    )
+
     def __str__(self):
         return f"{self.post} is liked by {self.liker}"
 
 
 class Follower(models.Model):
     following = models.ForeignKey(
-                        User,
-                        related_name='following',
-                        on_delete=models.CASCADE,
-                    )
+        User,
+        related_name="following",
+        on_delete=models.CASCADE,
+    )
     followed_by = models.ForeignKey(
-                        User,
-                        related_name='followed_by',
-                        on_delete=models.CASCADE,
-                    )
+        User,
+        related_name="followed_by",
+        on_delete=models.CASCADE,
+    )
+
     def __str__(self):
         return f"{self.followed_by} is following {self.following}"
--- network/views.py
+++ network/views.py
@@ -17,9 +17,9 @@ from network.models import PostForm, User, Post, Like, Follower
 
 def index(request):
     # Get all Posts
-    all_posts = (Post.objects.all()
-                    .annotate(like_count=Count('likes'))
-                    .order_by('-timestamp'))
+    all_posts = (
+        Post.objects.all().annotate(like_count=Count("likes")).order_by("-timestamp")
+    )
 
     # Explicitly declare to None, this hides all the [edit buttons] and [active like]
     # indicators in the index view if the user is AnonUser (aka unauthenticated user)
@@ -30,25 +30,25 @@ def index(request):
     # and which posts they own, then we filter the template like so:
     # if [posts] in [owned_posts] then show edit button
     if request.user.is_authenticated:
-        likes_by_user = (Like.objects.filter(liker=request.user))
+        likes_by_user = Like.objects.filter(liker=request.user)
         liked_posts = Post.objects.filter(likes__in=likes_by_user)
         owned_posts = Post.objects.filter(owner=request.user)
-    
+
     # Paginator, show ten (10) posts only per viewing, taken from django docs example
     # https://docs.djangoproject.com/en/3.1/topics/pagination/
     paginator = Paginator(all_posts, 10)
-    page_number = request.GET.get('page')
+    page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
 
     # Pass the contexts neatly :D
     context = {
-        'form': PostForm,
-        'all_posts': all_posts,
-        'liked_posts': liked_posts,
-        'owned_posts': owned_posts,
-        'page_obj': page_obj,
-        'page_number': page_number,
-        'page_range': paginator.page_range
+        "form": PostForm,
+        "all_posts": all_posts,
+        "liked_posts": liked_posts,
+        "owned_posts": owned_posts,
+        "page_obj": page_obj,
+        "page_number": page_number,
+        "page_range": paginator.page_range,
     }
 
     return render(request, "network/index.html", context)
@@ -67,9 +67,11 @@ def login_view(request):
             login(request, user)
             return HttpResponseRedirect(reverse("index"))
         else:
-            return render(request, "network/login.html", {
-                "message": "Invalid username and/or password."
-            })
+            return render(
+                request,
+                "network/login.html",
+                {"message": "Invalid username and/or password."},
+            )
     else:
         return render(request, "network/login.html")
 
@@ -88,18 +90,18 @@ def register(request):
         password = request.POST["password"]
         confirmation = request.POST["confirmation"]
         if password != confirmation:
-            return render(request, "network/register.html", {
-                "message": "Passwords must match."
-            })
+            return render(
+                request, "network/register.html", {"message": "Passwords must match."}
+            )
 
         # Attempt to create new user
         try:
             user = User.objects.create_user(username, email, password)
             user.save()
         except IntegrityError:
-            return render(request, "network/register.html", {
-                "message": "Username already taken."
-            })
+            return render(
+                request, "network/register.html", {"message": "Username already taken."}
+            )
         login(request, user)
         return HttpResponseRedirect(reverse("index"))
     else:
@@ -118,13 +120,13 @@ def new_post(request):
             return HttpResponseRedirect(reverse("index"))
         else:
             context = {
-                'form': PostForm,
-                }
+                "form": PostForm,
+            }
             return render(request, "network/new_post.html", context)
 
     else:
         context = {
-            'form': PostForm,
+            "form": PostForm,
         }
 
         return render(request, "network/new_post.html", context)
@@ -134,15 +136,16 @@ def new_post(request):
 def user_profile(request, profile_user_id):
     # Get the user requested in the url and their posts
     profile_user = User.objects.get(pk=profile_user_id)
-    posts = (Post.objects
-                    .filter(owner=profile_user)
-                    .annotate(like_count=Count('likes'))
-                    .order_by('-timestamp'))
+    posts = (
+        Post.objects.filter(owner=profile_user)
+        .annotate(like_count=Count("likes"))
+        .order_by("-timestamp")
+    )
 
     # Since the template uses these contexts by default, we also need to import this
     # Refactor to minimize db querying
     # Maybe we can remove this if the request is for a user profile and we can filter inline
-    likes_by_user = (Like.objects.filter(liker=request.user))
+    likes_by_user = Like.objects.filter(liker=request.user)
     liked_posts = Post.objects.filter(likes__in=likes_by_user)
     owned_posts = Post.objects.filter(owner=request.user)
 
@@ -150,28 +153,26 @@ def user_profile(request, profile_user_id):
     # Bet I can make an infinite scrolling version ala pinterest :P
     # Personally, in don't like pagination from a usability standpoint... :/
     paginator = Paginator(posts, 10)
-    page_number = request.GET.get('page')
+    page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
 
     # Get Follower + Following counts and list em out for easy access
     following = Follower.objects.filter(followed_by=profile_user)
     followers = Follower.objects.filter(following=profile_user)
     # Get a qset of users that follow the profile owner for templating
-    user_followers = (User.objects.filter(
-                                followed_by__in=followers
-    ))
+    user_followers = User.objects.filter(followed_by__in=followers)
 
     context = {
-        'profile_owner': profile_user,
-        'all_posts': posts,
-        'liked_posts': liked_posts,
-        'owned_posts': owned_posts,
-        'page_obj': page_obj,
-        'page_number': page_number,
-        'page_range': paginator.page_range,
-        'followers': followers,
-        'following': following,
-        'user_followers': user_followers,
+        "profile_owner": profile_user,
+        "all_posts": posts,
+        "liked_posts": liked_posts,
+        "owned_posts": owned_posts,
+        "page_obj": page_obj,
+        "page_number": page_number,
+        "page_range": paginator.page_range,
+        "followers": followers,
+        "following": following,
+        "user_followers": user_followers,
     }
 
     return render(request, "network/user_profile.html", context)
@@ -183,38 +184,37 @@ def follow_toggle(request):
         # Get the JSON data
         data = json.loads(request.body)
         to_follow_id = data.get("to_follow_id")
-        
+
         # The user object to follow
         u_to_follow = User.objects.filter(pk=to_follow_id).first()
-        f = (Follower.objects.filter(
-                            following=u_to_follow,
-                            followed_by=request.user)
-                            .first())
-        
+        f = Follower.objects.filter(
+            following=u_to_follow, followed_by=request.user
+        ).first()
+
         # Check if the follow already exists or not
         if f:
             f.delete()
-            return JsonResponse({
-                        "Status": "Success! User Unfollowed",
-                        "u_to_follow": u_to_follow.username,
-                        "new_follow": False,
-                        },
-                        status=200)
+            return JsonResponse(
+                {
+                    "Status": "Success! User Unfollowed",
+                    "u_to_follow": u_to_follow.username,
+                    "new_follow": False,
+                },
+                status=200,
+            )
         else:
-            Follower.objects.create(
-                            following=u_to_follow,
-                            followed_by=request.user)
-            return JsonResponse({
-                        "Status": "Success! User Followed",
-                        "u_to_follow": u_to_follow.username,
-                        "new_follow": True,
-                        },
-                        status=201)
+            Follower.objects.create(following=u_to_follow, followed_by=request.user)
+            return JsonResponse(
+                {
+                    "Status": "Success! User Followed",
+                    "u_to_follow": u_to_follow.username,
+                    "new_follow": True,
+                },
+                status=201,
+            )
 
     # Only accept PUT requests
-    return JsonResponse({
-        "error": "Invalid Request :("
-    }, status=400)
+    return JsonResponse({"error": "Invalid Request :("}, status=400)
 
 
 @login_required
@@ -226,91 +226,89 @@ def like_post(request):
         post = Post.objects.filter(pk=data.get("id")).first()
         user = request.user
 
-        like = Like.objects.filter(
-                                post=post,
-                                liker=user).first()
+        like = Like.objects.filter(post=post, liker=user).first()
         if not like:
             Like.objects.create(
-                        post=post,
-                        liker=user,
+                post=post,
+                liker=user,
             )
-            return JsonResponse({
-                            "like": "Post liked :)",
-                            "id": data.get("id"),
-                            "new_like": True
-                        }, 
-                        status=201)
+            return JsonResponse(
+                {"like": "Post liked :)", "id": data.get("id"), "new_like": True},
+                status=201,
+            )
         else:
             like.delete()
-            return JsonResponse({
-                            "like": "Post unliked",
-                            "id": data.get("id"),
-                            "new_like": False,                            
-                        },
-                        status=201)
-    
-    return JsonResponse({
-            "error": "Invalid Request :("
-        }, status=400)
+            return JsonResponse(
+                {
+                    "like": "Post unliked",
+                    "id": data.get("id"),
+                    "new_like": False,
+                },
+                status=201,
+            )
+
+    return JsonResponse({"error": "Invalid Request :("}, status=400)
 
 
 @login_required
 def edit_post(request):
-    if request.method =="PUT":
+    if request.method == "PUT":
         data = json.loads(request.body)
 
         post = Post.objects.filter(pk=data.get("id")).first()
         new_content = data.get("text")
-        
+
         if post.owner != request.user:
-            return JsonResponse({
-                        "error": "Forbidden, Only the post owner may edit a post",
-                        "owner": post.owner.id
-                        }, 
-                        status=403)
+            return JsonResponse(
+                {
+                    "error": "Forbidden, Only the post owner may edit a post",
+                    "owner": post.owner.id,
+                },
+                status=403,
+            )
 
         if post:
             post.text = new_content
             post.save()
-            return JsonResponse({
-                        "edit": "Post edited :)",
-                        "new_content": new_content,
-                        }, 
-                        status=201)
+            return JsonResponse(
+                {
+                    "edit": "Post edited :)",
+                    "new_content": new_content,
+                },
+                status=201,
+            )
         else:
-            return JsonResponse({
-                        "error": "Invalid Post"
-                        }, 
-                        status=400)
+            return JsonResponse({"error": "Invalid Post"}, status=400)
 
     return JsonResponse({"error": "Invalid Request"}, status=400)
 
 
 @login_required
 def delete_post(request):
-    if request.method =="PUT":
+    if request.method == "PUT":
         data = json.loads(request.body)
 
         post = Post.objects.filter(pk=data.get("id")).first()
-        
+
         if post.owner != request.user:
-            return JsonResponse({
-                        "error": "Forbidden, Only the owner may edit a post",
-                        "owner": post.owner.id
-                        }, 
-                        status=403)
+            return JsonResponse(
+                {
+                    "error": "Forbidden, Only the owner may edit a post",
+                    "owner": post.owner.id,
+                },
+                status=403,
+            )
 
         if post:
             post.delete()
-            return JsonResponse({
-                        "edit": "Post deleted!",
-                        }, 
-                        status=201)
+            return JsonResponse(
+                {
+                    "edit": "Post deleted!",
+                },
+                status=201,
+            )
         else:
-            return JsonResponse({
-                        "error": "Invalid Post"
-                        }, 
-                        status=400)
+            return JsonResponse({"error": "Invalid Post"}, status=400)
 
     return JsonResponse({"error": "Invalid Request"}, status=400)
 
@@ -320,30 +318,31 @@ def following(request):
     # Get the user requested in the url and their posts
     follows_by_user = Follower.objects.filter(followed_by=request.user)
     followed_users = User.objects.filter(following__in=follows_by_user)
-    posts = (Post.objects
-                    .filter(owner__in=followed_users)
-                    .annotate(like_count=Count('likes'))
-                    .order_by('-timestamp'))
+    posts = (
+        Post.objects.filter(owner__in=followed_users)
+        .annotate(like_count=Count("likes"))
+        .order_by("-timestamp")
+    )
 
     # Owned is none, since this view is for posts by followed users
     # We own nothing here!
-    likes_by_user = (Like.objects.filter(liker=request.user))
+    likes_by_user = Like.objects.filter(liker=request.user)
     liked_posts = Post.objects.filter(likes__in=likes_by_user)
     owned_posts = None
 
     # We meet again mr. paginator
     paginator = Paginator(posts, 10)
-    page_number = request.GET.get('page')
+    page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
 
     # Nice and neat!
     context = {
-        'all_posts': posts,
-        'liked_posts': liked_posts,
-        'owned_posts': owned_posts,
-        'page_obj': page_obj,
-        'page_number': page_number,
-        'page_range': paginator.page_range,
+        "all_posts": posts,
+        "liked_posts": liked_posts,
+        "owned_posts": owned_posts,
+        "page_obj": page_obj,
+        "page_number": page_number,
+        "page_range": paginator.page_range,
     }
 
     return render(request, "network/following.html", context)
